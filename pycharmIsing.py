7a2e59ff58e244ab4eb80298adca88bb706e89c1
tperez35 <tperez35@gatech.edu>
2022-11-14 21:35:44 -0800

    Ising eg Day-1

---
+++ pycharmIsing.py
@@ -0,0 +1,35 @@
+
+# ISING eg day-1
+# https://support.dwavesys.com/hc/en-us/articles/360003718693-Define-a-Simple-Problem-and-Submit-It-to-a-QPU-Ising-Example-
+# $ pip install dimod                           # !
+# $ pip install dwave-system                    # !
+
+from dwave.system import DWaveSampler           # ! >>>
+from dwave.system import EmbeddingComposite
+
+print('\nHello Ising Example')
+solver = DWaveSampler()
+
+sampler = EmbeddingComposite(solver)
+print('\n', solver.properties.keys())          # was 'solver'
+
+
+qubits = solver.properties['qubits']
+couplers = solver.properties['couplers']        # was 'solver'
+
+qubit_1 = 0
+qubit_2 = 4
+coupler = [qubit_1, qubit_2]
+
+if (qubit_1 in qubits) and \
+   (qubit_2 in qubits) and \
+   (coupler in couplers):
+    print('\nQubits {} and {} and their coupler {} are available'.format(qubit_1, qubit_2, coupler))
+
+else:
+   print('\nNo qubit_1, qubit_2, and coupler\n')
+
+h = {qubit_1:-1,qubit_2:-1}
+J = {tuple(coupler): -1}
+
+solution = sampler.sample_ising(h, J, num_reads=10)    # was 'solver'
